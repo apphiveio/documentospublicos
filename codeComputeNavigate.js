eb630da9fcbacf1ec515b20c69308d2649fecc02
Jorge Rangel <jorgerangel@192.168.1.5>
2024-02-16 12:27:51 -0600

    Update navegar compute

--- codeComputeNavigate.js
+++ codeComputeNavigate.js
@@ -3,21 +3,22 @@ const {
     parametrosDePagina,
 } = args
 
-const a = apphive.fn.callCallback
-const b = (mensaje) => a('onParametrosDePaginaIncorrectos', mensaje)
-const c = (valor) => typeof valor !== 'string' || !valor
+const irAPagina = apphive.fn.callCallback
+const irAErrorParametrosErroneos = (mensaje) => irAPagina('onParametrosDePaginaIncorrectos', mensaje)
+const valorNoEsUnaCadenaNoVacia = (valor) => typeof valor !== 'string' || !valor
 
 const valorNoEsUnRecord = (valor) => typeof valor !== 'object' || valor === null
+const valorNoEsUnArreglo = (valor) => !Array.isArray(valor)
 
 const encontrarErrorEnCorreoYPassword = (objetoOriginal) => {
     if (valorNoEsUnRecord(objetoOriginal)) {
         return `Los parametros deben ser un objeto, se paso eso : ${JSON.stringify(parametrosDePagina)}`
     }
     const { direccionDeCorreo, password } = objetoOriginal
-    if (c(direccionDeCorreo)) {
+    if (valorNoEsUnaCadenaNoVacia(direccionDeCorreo)) {
         return `La direcccion de correo no es valida : ${JSON.stringify(parametrosDePagina)}`
     }
-    if (c(password)) {
+    if (valorNoEsUnaCadenaNoVacia(password)) {
         return `El password no es valido : ${JSON.stringify(parametrosDePagina)}`
     }
     return undefined
@@ -25,63 +26,75 @@ const encontrarErrorEnCorreoYPassword = (objetoOriginal) => {
 
 switch (paginaObjetivo) {
 case 'pedirPasswordParaNuevaCuenta': {
-    if (c(parametrosDePagina)) {
-        b(`Para pedir password de nueva cuenta se requiere un correo valido, se paso eso : ${JSON.stringify(parametrosDePagina)}`)
+    if (valorNoEsUnaCadenaNoVacia(parametrosDePagina)) {
+        irAErrorParametrosErroneos(`Para pedir password de nueva cuenta se requiere un correo valido, se paso eso : ${JSON.stringify(parametrosDePagina)}`)
     } else {
-        a('pedirPasswordParaNuevaCuenta', parametrosDePagina)
+        irAPagina('pedirPasswordParaNuevaCuenta', parametrosDePagina)
     }
     break
 }
 case 'validarCorreoEnFirebase': {
-    if (c(parametrosDePagina)) {
-        b(`Para validar en firebase el correo debe ser una cadena de caracteres, se paso eso : ${JSON.stringify(parametrosDePagina)}`)
+    if (valorNoEsUnaCadenaNoVacia(parametrosDePagina)) {
+        irAErrorParametrosErroneos(`Para validar en firebase el correo debe ser una cadena de caracteres, se paso eso : ${JSON.stringify(parametrosDePagina)}`)
     } else {
-        a('validarCorreoEnFirebase', parametrosDePagina)
+        irAPagina('validarCorreoEnFirebase', parametrosDePagina)
     }
     break
 }
 case 'pedirConfirmacionPassword': {
     const mensajeDeError = encontrarErrorEnCorreoYPassword(parametrosDePagina)
     if (mensajeDeError) {
-        b(mensajeDeError)
+        irAErrorParametrosErroneos(mensajeDeError)
         break
     }
 
-    a('pedirConfirmacionPassword', parametrosDePagina)
+    irAPagina('pedirConfirmacionPassword', parametrosDePagina)
     break
 }
 case 'pedirCorreoParaEntrar':
-    a('pedirCorreoParaEntrar')
+    irAPagina('pedirCorreoParaEntrar')
     break
 case 'crearCuentaConCorreoYPassword': {
     const mensajeDeError = encontrarErrorEnCorreoYPassword(parametrosDePagina)
     if (mensajeDeError) {
-        b(mensajeDeError)
+        irAErrorParametrosErroneos(mensajeDeError)
         break
     }
-    a('crearCuentaConCorreoYPassword', parametrosDePagina)
+    irAPagina('crearCuentaConCorreoYPassword', parametrosDePagina)
     break
 }
 case 'cargarCorreoEnFormulario':
-    a('cargarCorreoEnFormulario')
+    irAPagina('cargarCorreoEnFormulario')
     break
 // mensaje titulo
 case 'error': {
     if (valorNoEsUnRecord(parametrosDePagina)) {
-        b(`Los parametros no son un record para mostrar pagina de error ${JSON.stringify(parametrosDePagina)}`)
+        irAErrorParametrosErroneos(`Los parametros no son un record para mostrar pagina de error ${JSON.stringify(parametrosDePagina)}`)
         break
     }
     const { mensaje, titulo } = parametrosDePagina
-    if (c(titulo)) {
-        b(`Cuando se muestra un error se requiere que el titulo sea una cadena y esto se encontro: ${titulo}`)
+    if (valorNoEsUnaCadenaNoVacia(titulo)) {
+        irAErrorParametrosErroneos(`Cuando se muestra un error se requiere que el titulo sea una cadena y esto se encontro: ${titulo}`)
         break
     }
-    a('error', { mensaje, titulo })
+    irAPagina('error', { mensaje, titulo })
+    break
+}
+case 'administradores': {
+    if (valorNoEsUnArreglo(parametrosDePagina)) {
+        irAErrorParametrosErroneos('Para mostrar la lista de administradores se requiere el arreglo de los mismos')
+        break
+    }
+    irAPagina(paginaObjetivo, parametrosDePagina)
     break
 }
 case 'validarUsuarioEsAdmin':
-    a('validarUsuarioEsAdmin')
+case 'correoNoPuedeSerAdmin':
+case 'splash':
+case 'principal':
+case 'cargandoAdministradores':
+    irAPagina(paginaObjetivo)
     break
 default:
-    a('onPaginaObjetivoInvalida')
+    irAPagina('onPaginaObjetivoInvalida')
 }
